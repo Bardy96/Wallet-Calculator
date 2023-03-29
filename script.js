b4d168e68c8d8b611c1ce6b1e01c56eec8ba46c5
Wojtek Bardyga <wojtek.bardyga@gmail.com>
2023-03-29 22:29:25 +0200

    Add new functions

--- script.js
+++ script.js
@@ -19,28 +19,73 @@ let categoryIcon;
 let selectedCategory;
 let moneyArr = [0]
 
-const showPanel = () => { 
-    addTransactionPanel.style.display = 'flex'
- }
+const showPanel = () => {
+    addTransactionPanel.style.display = 'flex';
+}
 
- const closePanel = () => { 
-    addTransactionPanel.style.display = 'none'
-    clearInputs()
-  }
+const closePanel = () => {
+    addTransactionPanel.style.display = 'none';
+    clearInputs();
+}
 
-  const checkForm = () => {
-    if(nameInput.value !== '' && amountInput.value !== '' && categorySelect.value !== 'none') {
-        console.log('ok')
+const checkForm = () => {
+    if (nameInput.value !== '' && amountInput.value !== '' && categorySelect.value !== 'none') {
+        createNewTransaction();
     } else {
-        alert('wypełnij wszystko')
+        alert('Wypełnij wszystkie pola!')
     }
-  }
+}
 
-  const clearInputs = () => {
-    nameInput.value = '' 
-    amountInput.value = '' 
-    categorySelect.selectedIndex = 0
-  }
+const clearInputs = () => {
+    nameInput.value = '';
+    amountInput.value = '';
+    categorySelect.selectedIndex = 0;
+}
+
+
+const createNewTransaction = () => {
+    const newTransaction = document.createElement('div');
+    newTransaction.classList.add('transaction');
+    newTransaction.setAttribute('id', ID);
+    checkCategory(selectedCategory);
+
+    newTransaction.innerHTML = `
+        <p class="transaction-name">
+        ${categoryIcon} ${nameInput.value}
+        </p>
+        <p class="transaction-amount">
+        ${amountInput.value}zł 
+        <button class="delete" onclick="deleteTransatcion(${ID})"><i class="fas fa-times"></i></button>
+        </p>
+    `;
+
+    amountInput.value > 0 ? incomeSection.appendChild(newTransaction) && newTransaction.classList.add('income') : expensesSection.appendChild(newTransaction) && newTransaction.classList.add('expense');
+    closePanel();
+    ID++;
+    clearInputs();
+}
+
+const selectCategory = () => {
+    selectedCategory = categorySelect.options[categorySelect.selectedIndex].text;
+}
+
+
+const checkCategory = transaction => {
+    switch (transaction) {
+        case '[ + ] Income':
+            categoryIcon = '<i class="fas fa-money-bill-wave"></i>'
+            break;
+        case '[ - ] Shopping':
+            categoryIcon = '<i class="fas fa-cart-arrow-down"></i>'
+            break;
+        case '[ - ] Food':
+            categoryIcon = '<i class="fas fa-hamburger"></i>'
+            break;
+        case '[ - ] Cinema':
+            categoryIcon = '<i class="fas fa-film"></i>'
+            break;
+    }
+}
 
 addTransactionBtn.addEventListener('click', showPanel)
 cancelBtn.addEventListener('click', closePanel)
